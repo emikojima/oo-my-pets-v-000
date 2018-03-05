47d6614c639929f6fe7658862103b7a31cf81a36
emi kojima <emi8emi@gmail.com>
2018-03-05 20:34:33 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,11 @@
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+ 
+  
+  def initialize(dog_name)
+    @name = dog_name
+    @mood = "nervous"
+   
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+ 
+  def initialize(name)
+    @@all << self
+    @species = "human"
+    @pets = {:fishes =>[], :dogs => [], :cats => []}
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+  def self.count 
+    @@all.size
+  end 
+  
+  def say_species 
+    "I am a #{@species}."
+  end
+  
 end
