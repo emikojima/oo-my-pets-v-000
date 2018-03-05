1440b5bee509072428ee3b18a6017e8aff62e547
emi kojima <emi8emi@gmail.com>
2018-03-05 21:51:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,6 +56,10 @@ class Owner
     end 
   end 
   
+  def sell_pets
+    
+  end 
+  
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cats(s)."
   end 
